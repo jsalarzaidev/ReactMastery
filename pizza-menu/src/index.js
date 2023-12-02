68ca751666db8928ce67df96798fe666159a551f
jsalarzaidev <jahanzaibsalarzai.dev@outlook.com>
2023-12-02 20:21:10 +0500

    Creating and Reusing Component

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -1,10 +1,71 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
 function App() {
   // App Component
-  const x = "jonas";
-  return <h1>This is from App</h1>;
+  return (
+    <div>
+      <h1>This is from App</h1>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <h2>Pizza Focaccia</h2>
+      <p>Bread with italian olive oil and rosemary</p>
+      <img src="pizzas/focaccia.jpg" alt="focaccia" />
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
